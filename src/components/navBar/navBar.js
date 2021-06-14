7e37d89db9356131999e4834bbb97d2ca05b536f
flaviamald88 <fla_m19@hotmail.com>
2021-06-14 01:32:33 -0300

    Se agregaron cambios al navbar

---
+++ src/components/navBar/navBar.js
@@ -0,0 +1,14 @@
+export const NavBar = () => {
+
+    return (
+        <nav>
+            <ul className="navbar-container">
+                <li>Home</li>
+                <li>Gafas</li>
+                <li>Pañuelos</li>
+                <li>Cositas para el hogar</li>
+                <li>Carrito</li>
+            </ul>
+        </nav>
+    )
+}
