f53c899e677c191b5b70678667983340b3de708f
Francisco Rodriguez <fjrodriguez.353@gmail.com>
2021-08-13 12:30:44 -0300

    fix by franncode

--- src/pages/ItemListContainer/ItemListContainer.js
+++ src/pages/ItemListContainer/ItemListContainer.js
@@ -5,38 +5,42 @@ import { useParams } from 'react-router-dom'
 import { Loading } from '../../components/Loading/loading'
 import { dataBase } from '../../Firebase/firebase'
 
-
 export const ItemListContainer = (props) => {
-   const { categoryId } = useParams()
-   const [loading, setLoading] = useState(true)
-   const [catalogoProductos, setCatalogoProductos] = useState([])
- 
- 
-   useEffect(()=> {
-      const db = dataBase
-      const itemCollection = db.collection("productos")
-        let listaDeItems
-        categoryId ? listaDeItems = itemCollection.where("category", "==", categoryId) : listaDeItems = itemCollection  
-        listaDeItems.get().then(querySnapshot =>{
-        setCatalogoProductos(querySnapshot.docs.map(doc=>({id: doc.id, ...doc.data()})))
-          }).catch((error)=> {
-             console.log("Hubo un error al cargar los productos", error);
-          }).finally(()=> {
-          setLoading(false);
-          }); 
-    }, [categoryId]);
+	const { categoryId } = useParams()
+	const [loading, setLoading] = useState(true)
+	const [catalogoProductos, setCatalogoProductos] = useState([])
 
-    return (
-       <section>
-          <h1 className="title">{props.greeting}</h1>
-          <h2>{props.greeting2}</h2>
-           {
-              loading
-              ? <Loading/>
-              : <ItemList catalogoProductos= {catalogoProductos}/>
-           }
-       </section>
-        
-      
-    )
+	useEffect(() => {
+		const db = dataBase
+		const itemCollection = db.collection('productos')
+		let listaDeItems
+		categoryId
+			? (listaDeItems = itemCollection.where('category', '==', categoryId))
+			: (listaDeItems = itemCollection)
+		listaDeItems
+			.get()
+			.then((querySnapshot) => {
+				setCatalogoProductos(
+					querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+				)
+			})
+			.catch((error) => {
+				console.log('Hubo un error al cargar los productos', error)
+			})
+			.finally(() => {
+				setLoading(false)
+			})
+	}, [categoryId])
+
+	return (
+		<section>
+			<h1 className='title'>{props.greeting}</h1>
+			<h2>{props.greeting2}</h2>
+			{loading ? (
+				<Loading />
+			) : (
+				<ItemList catalogoProductos={catalogoProductos} />
+			)}
+		</section>
+	)
 }
